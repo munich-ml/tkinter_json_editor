2b843bf2e9aa4dd38fac6b92faf6d52a4c10d32b
Holger <holger.steffens@gmail.com>
2022-09-12 15:44:03 +0200

    load_json_file implemented

--- src/tkinter_json_editor.py
+++ src/tkinter_json_editor.py
@@ -3,6 +3,8 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog, font, messagebox, Tk
 
+
+
 class JSONTreeFrame(ttk.Frame):
 
     def __init__(self, master):
@@ -11,7 +13,7 @@ class JSONTreeFrame(ttk.Frame):
         self.control_frame = ttk.Frame(self)
         self.control_frame.pack(fill=tk.X)
         tk.Label(self.control_frame, text="control_frame").pack(side=tk.LEFT)
-        ttk.Button(self.control_frame, text="Button 1", command=lambda: print("hi")).pack(side=tk.RIGHT)
+        ttk.Button(self.control_frame, text="load JSON file", command=self.load_json_file).pack(side=tk.RIGHT)
         
         tree_frame = ttk.Frame(self)
         tree_frame.pack(fill=tk.BOTH, expand=True)
@@ -24,7 +26,23 @@ class JSONTreeFrame(ttk.Frame):
         self.tree.configure(yscroll=ysb.set)
         ysb.grid(row=0, column=1, sticky="ns")        
 
+
+    def load_json_file(self):
+        fp = filedialog.askopenfilename(filetypes=[("JSON files", "*.json"), ("All Files", "*.*")])
+        if fp is None:
+            return
         
+        try:
+            with open(fp, "r") as file:
+                obj = json.load(file)
+        except Exception as e:
+            messagebox.showwarning(title="Warning", message=f"Could not open '{fp}'!")
+            return
+        
+        print(fp)
+        print(obj)
+
+
 if __name__ == '__main__':
     app = tk.Tk()
     app.title('Tkinter JSON Editor')
