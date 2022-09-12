bc5f59f3c084de6bc297700636d0bc60d633ddd4
Holger <holger.steffens@gmail.com>
2022-09-12 15:16:55 +0200

    refactoring

--- src/tkinter_json_editor.py
+++ src/tkinter_json_editor.py
@@ -7,10 +7,11 @@ class JSONTreeFrame(ttk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
+        self.pack(fill=tk.BOTH, expand=True)
         self.control_frame = ttk.Frame(self)
         self.control_frame.pack(fill=tk.X)
         tk.Label(self.control_frame, text="control_frame").pack(side=tk.LEFT)
-        ttk.Button(self.control_frame, text="Button 1").pack(side=tk.RIGHT)
+        ttk.Button(self.control_frame, text="Button 1", command=lambda: print("hi")).pack(side=tk.RIGHT)
         
         tree_frame = ttk.Frame(self)
         tree_frame.pack(fill=tk.BOTH, expand=True)
@@ -18,17 +19,15 @@ class JSONTreeFrame(ttk.Frame):
         tree_frame.columnconfigure(0, weight=1)
         
         self.tree = ttk.Treeview(tree_frame)
-        #self.tree.pack(fill=tk.BOTH, expand=True)
-        self.tree.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.tree.grid(row=0, column=0, sticky="nsew")
         ysb = ttk.Scrollbar(tree_frame, orient=tk.VERTICAL, command=self.tree.yview)
         self.tree.configure(yscroll=ysb.set)
-        ysb.grid(row=0, column=1, sticky=(tk.N, tk.S))        
+        ysb.grid(row=0, column=1, sticky="ns")        
 
         
 if __name__ == '__main__':
     app = tk.Tk()
     app.title('Tkinter JSON Editor')
     app.geometry("500x500")
-    jtf = JSONTreeFrame(app)
-    jtf.pack(fill=tk.BOTH, expand=True)
+    JSONTreeFrame(app)
     app.mainloop()
